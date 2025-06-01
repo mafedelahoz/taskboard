102128258e7b5e0a6d1418d95ca9f87bebfa8edb
mafedelahoz <m.delah@uniandes.edu.co>
2025-06-01 12:11:49 -0500

    Fix import

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from '../../../lib/prisma';
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/auth.config';
+import { authOptions } from '../auth/[...nextauth]/route';
 
 export async function GET() {
   try {
