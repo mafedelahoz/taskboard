8a13875cceea88d2bdf5317b6ce9cee3cdb304a4
mafedelahoz <m.delah@uniandes.edu.co>
2025-06-01 13:35:03 -0500

    Fix authentication

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,10 +1,11 @@
 import { prisma } from '../../../lib/prisma';
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/auth.config';
 
 export async function GET() {
   try {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
 
     if (!session?.user?.id) {
       return NextResponse.json(
@@ -35,7 +36,7 @@ export async function GET() {
 
 export async function POST(request: Request) {
   try {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
 
     if (!session?.user?.id) {
       return NextResponse.json(
