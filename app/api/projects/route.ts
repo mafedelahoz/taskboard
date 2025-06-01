ab665eb39064531789a15a6c5031111ac68f02dc
mafedelahoz <m.delah@uniandes.edu.co>
2025-06-01 12:23:16 -0500

    Auth directory

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from '../../../lib/prisma';
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/options';
+import { authOptions } from '../auth/config';
 
 export async function GET() {
   try {
--- app/projects/route.ts
+++ app/projects/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { prisma } from '../../lib/prisma'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '../api/auth/options'
+import { authOptions } from '../api/auth/config'
 
 export async function POST(request: Request) {
   try {
